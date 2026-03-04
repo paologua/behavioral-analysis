79afe01bd72bc09942cd456b370b942a97312c2a
paologua <nex.dpg@gmail.com>
2026-03-04 10:59:36 +0100

    Add InferenceEngine class with analizza method

---
+++ core/inference_engine.js
@@ -0,0 +1,10 @@
+ class InferenceEngine {
+    analizza(input) {
+        return {
+            pattern: { principale: 'Stress Cognitivo', score: 78 },
+            intensita: 65,
+            direzione: 'fuga'
+        };
+    }
+}
+module.exports = InferenceEngine;
