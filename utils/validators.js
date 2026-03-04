a6abe899d603c4c6de2849506f1f0f87dd3e1c38
paologua <nex.dpg@gmail.com>
2026-03-04 11:44:04 +0100

    Refactor peso validation and add context validation

--- utils/validators.js
+++ utils/validators.js
@@ -1,6 +1,32 @@
 class Validators {
     static validaPeso(peso) {
-        return peso >= 0 && peso <= 3;
+        const p = parseInt(peso);
+        return !isNaN(p) && p >= 0 && p <= 3;
+    }
+
+    static validaContesto(contesto) {
+        return ['formale', 'informale', 'negoziale', 'conflittuale', 'intimo'].includes(contesto);
+    }
+
+    static validaOsservazione(osservazione, segnali) {
+        const errors = [];
+        
+        Object.entries(osservazione).forEach(([id, peso]) => {
+            const segnale = segnali.find(s => s.id === id);
+            if (!segnale) {
+                errors.push(`Segnale ${id} non trovato`);
+            } else if (!this.validaPeso(peso)) {
+                errors.push(`Peso ${peso} non valido`);
+            }
+        });
+
+        return {
+            isValid: errors.length === 0,
+            errors
+        };
     }
 }
-module.exports = Validators;
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Validators;
+}
